70248f213852afe7cd3348fb1a044019ec479ccc
dolga <dolganov-ninkita@inbox.ru>
2024-01-21 15:59:14 +0300

    lesson 2 version 1.0

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ import java.security.spec.RSAOtherPrimeInfo;
 
 public class Main {
     public static void main(String[] args) {
-    var dog = 8.0;
-    var cat = 3.6;
-    var paper = 763789;
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
         System.out.println("Задача 1");
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
 
-    var dog2 = dog + 4;
-    var cat2 = cat + 4;
-    var paper2 = paper + 4;
+        var dog2 = dog + 4;
+        var cat2 = cat + 4;
+        var paper2 = paper + 4;
         System.out.println("Задача 2");
         System.out.println(dog2);
         System.out.println(cat2);
